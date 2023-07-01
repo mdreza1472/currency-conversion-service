ad8768966c57c69d4044c5fce24b8244358315a4
mdreza1472 <mdreza1472@gmail.com>
2023-07-01 18:28:22 +0400

    initial commit

---
+++ currency-conversion-service/src/main/java/com/techiereza/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -0,0 +1,19 @@
+package com.techiereza.microservices.currencyconversionservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+//@FeignClient(name="currency-exchange", url="localhost:8000")
+//-@FeignClient(name="currency-exchange")
+//CHANGE KUBERNETES - CURRENCY_EXCHANGE_SERVICE_HOST will be an environment variable.
+@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000")
+public interface CurrencyExchangeProxy {
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversion retrieveExchangeValue(
+			@PathVariable String from,
+			@PathVariable String to);
+
+}
