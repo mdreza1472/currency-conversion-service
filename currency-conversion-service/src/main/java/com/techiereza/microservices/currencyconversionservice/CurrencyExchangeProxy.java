96675b738e873c5d7392fdb8f889ca8a302aa007
mdreza1472 <mdreza1472@gmail.com>
2023-07-03 23:18:15 +0400

    changes pertaining to declarative approach - yaml

--- currency-conversion-service/src/main/java/com/techiereza/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/techiereza/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 //@FeignClient(name="currency-exchange", url="localhost:8000")
 //-@FeignClient(name="currency-exchange")
 //CHANGE KUBERNETES - CURRENCY_EXCHANGE_SERVICE_HOST will be an environment variable.
-@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000")
+//@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000")
+@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_URI:http://localhost}:8000")//0.0.12 version
 public interface CurrencyExchangeProxy {
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
